97fc368c3b38a0b2ed4775658f58bae0a6410203
masoares <manueljorgecruz@hotmail.com>
2024-08-14 10:07:59 +0100

    start of tests

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: masoares <masoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 09:57:38 by luis-ffe          #+#    #+#             */
-/*   Updated: 2024/08/14 10:07:13 by masoares         ###   ########.fr       */
+/*   Updated: 2024/08/14 10:07:42 by masoares         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -43,16 +43,6 @@ int	main(int argc, char **argv)
 	read_lines(&cub);
 	parser_first(&cub);
 
-	return (0);
-	(void) argc;
-	(void) argv;
-	// check_user_input(argc, argv[1], &cub);
-	// /*testing GNL */
-	
-	// ft_printf("[1]\n");
-	// read_mapfile(&cub, argv[1]);
-	// ft_printf("[2]\n");
-	
 	/*coment this up here if you need */	
 	init_fields(&cub);
 	//printf("%c",cub.north[2]);
